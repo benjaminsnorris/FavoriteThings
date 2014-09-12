15ccd8d3a98c2b27504d270f4210d60cc95a316b
Ben Norris <ben@bsn.io>
2014-09-11 18:31:01 -0600

    Code for test (up to the end of step 2 in README)

---
+++ FavoriteThings-Objc/FTViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTViewController.h
+//  FavoriteThings-Objc
+//
+//  Created by Ben Norris on 9/11/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTViewController : UIViewController
+
+@end
---
+++ FavoriteThings-Objc/FTViewController.m
@@ -0,0 +1,55 @@
+//
+//  FTViewController.m
+//  FavoriteThings-Objc
+//
+//  Created by Ben Norris on 9/11/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import "FTViewController.h"
+
+@interface FTViewController ()
+
+@property (nonatomic, strong) UILabel *label;
+
+@end
+
+@implementation FTViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(15, 150, 200, 30)];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [button setTitle:@"Touch me" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(changeLabelText) forControlEvents:UIControlEventTouchUpInside];
+    
+    self.label = [[UILabel alloc] initWithFrame:CGRectMake(15, 200, 200, 30)];
+    self.label.text = @"Change me";
+    self.label.textAlignment = NSTextAlignmentCenter;
+    
+    [self.view addSubview:button];
+    [self.view addSubview:self.label];
+}
+
+- (void)changeLabelText {
+    self.label.text = @"Changed";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
